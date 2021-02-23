c7fb7322d0b9271c91bf7f3c65aed15ccc59bfe2
WhatTheFar <jakpat.m@gmail.com>
2021-02-23 13:02:05 +0700

    test(nest): undefined user controller

--- apps/nest-opaque-error/src/user/interface/user.controller.spec.ts
+++ apps/nest-opaque-error/src/user/interface/user.controller.spec.ts
@@ -1,13 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { CommonModule } from '../../common/common.module';
+import { UserInteractorInput } from '../domain/user.interactor';
 import { UserController } from './user.controller';
+
+class MockInteractor implements UserInteractorInput {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public createUser(_req: {
+    username: string;
+    password: string;
+  }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public requestToken(_req: {
+    username: string;
+    password: string;
+  }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
 
 describe('User Controller', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CommonModule],
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserInteractorInput,
+          useClass: MockInteractor,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
